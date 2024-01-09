3676d84fcdbba03b50c280a15df9038814924245
sky-02 <138518519+sky-02@users.noreply.github.com>
2024-01-10 00:31:22 +0530

    Update RepositoryDetailsController.java

--- src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
+++ src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
@@ -34,7 +34,7 @@ public class RepositoryDetailsController {
 	public String getRepos() throws IOException {
 		GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
 		GHRepositorySearchBuilder builder = github.searchRepositories();
-		return "Greetings from Valaxy. Deployment from helm";
+		return "My first Project on EKS";
 	}
 
 	@GetMapping("/trends")
